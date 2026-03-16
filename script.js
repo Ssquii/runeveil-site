d79cbc37472a0aebad80480ffc9a0fc20d5b23f6
Ssquii <chase-orr@hotmail.com>
2026-03-15 22:33:13 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
-function copyIP() {
-  const ip = document.getElementById("serverip");
-  if (!ip) return;
-  navigator.clipboard.writeText(ip.innerText).then(() => {
-    document.querySelectorAll(".btn-primary").forEach((btn) => {
-      const old = btn.innerText;
-      btn.innerText = "Copied IP";
-      setTimeout(() => btn.innerText = old, 1400);
-    });
-  }).catch(() => {
-    alert("Copy failed. Server IP: " + ip.innerText);
-  });
-}
 
-function toggleNav() {
-  const nav = document.getElementById("navLinks");
-  if (nav) nav.classList.toggle("open");
+function copyIP() {
+  const ip = "142.44.144.84";
+
+  navigator.clipboard.writeText(ip).then(function () {
+    alert("Server IP copied: " + ip);
+  }).catch(function () {
+    const temp = document.createElement("input");
+    temp.value = ip;
+    document.body.appendChild(temp);
+    temp.select();
+    document.execCommand("copy");
+    document.body.removeChild(temp);
+    alert("Server IP copied: " + ip);
+  });
 }
